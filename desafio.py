0d2ee543c58222072540f08a89b61d88a7e01122
Manuel Castro <castroavila@ic.unicamp.br>
2025-06-13 22:15:41 -0300

    Depósitos/saques stored in lists. Additional information shown in statement.

--- desafio.py
+++ desafio.py
@@ -12,16 +12,19 @@ menu = """
 [d] Depositar
 [s] Sacar
 [e] Extrato
+[i] Aumentar limite saques
 [q] Sair
 
 => """
 
 saldo = 0
 limite = 500
-extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
 
+depositos = []
+saques = []
+movimentos = False
 while True:
 
     opcao = input(menu)
@@ -31,7 +34,8 @@ while True:
 
         if valor > 0:
             saldo += valor
-            extrato += f"Depósito: R$ {valor:.2f}\n"
+            depositos.append(valor)
+            movimentos = True
 
         else:
             print("Operação falhou! O valor informado é inválido.")
@@ -56,18 +60,33 @@ while True:
 
         elif valor > 0:
             saldo -= valor
-            extrato += f"Saque: R$ {valor:.2f}\n"
+            saques.append(valor)
             numero_saques += 1
 
         else:
             print("Operação falhou! O valor informado é inválido.")
 
+    elif opcao == "i":
+        atualizar_numero_saques = int(input('Novo numero máximo de saques: '))
+        LIMITE_SAQUES = atualizar_numero_saques
+        print(f'Número maximo de saques atualizado para: {LIMITE_SAQUES}')
     elif opcao == "e":
         print("\n================ EXTRATO ================")
-        print(
-            "Não foram realizadas movimentações." if not extrato else extrato
-        )
+        if movimentos:
+            if len(depositos) > 0:
+                print('........Depositos...........')
+                for i, deposito in enumerate(depositos):
+                    print(f'Deposito {i+1}: R$: {deposito:0.2f}')
+            if len(saques) > 0:
+                print('........Saques...........')
+                for i, saque in enumerate(saques):
+                    print(f'Saque {i+1}: R$: {saque:0.2f}')
+        else:
+            print("Não foram realizadas movimentações.")
+
         print(f"\nSaldo: R$ {saldo:.2f}")
+        print(f"Número de saques até agora: {numero_saques}")
+        print(f"Numero máximo de saques: {LIMITE_SAQUES}")
         print("==========================================")
 
     elif opcao == "q":
